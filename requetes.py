b06d8fb7fff21efc4a9edd911221f4ff6b128a4b
RacimRgh <racim458@gmail.com>
2020-06-27 17:33:28 +0100

    added query as function

--- requetes.py
+++ requetes.py
@@ -3,28 +3,38 @@ import rdflib
 graph = rdflib.Graph()
 graph.parse("maladies.owl", format='turtle')
 graph.serialize("maladies_out.rdf", "turtle")
-prop = "présente"
-requete = f""" 
-prefix ns1: <http://www.semanticweb.org/racim_katia/maladies.owl#>
-prefix ns2: <http://www.w3.org/2002/07/owl#>
-prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-prefix xsd: <http://www.w3.org/2001/XMLSchema#>
-prefix xml: <http://www.w3.org/XML/1998/namespace>
-SELECT ?id ?maladie ?nom
-WHERE{{
-    ?code rdf:type ns1:Patient .
-    ?code ns1:ID ?id .
-    ?code rdf:type ns1:Patient .
-    ?code ns1:{prop} ?maladie .
-    ?code rdf:type ns1:Patient .
-    ?code ns1:Nom ?nom
-}}"""
-# ?nom rdf:type ns1:Femme .
-#     ?nom ns1:Nom ?name .
-#     ?pren rdf:type ns1:Femme .
-#     ?pren ns1:Prenom ?prenom .
-resultat = graph.query(requete)
-for i in list(resultat):
-    # print(list(i)[1])
-    print(i)
+
+
+def get_props(prop, value=None):
+    requete = f"""
+    prefix ns1: <http://www.semanticweb.org/racim_katia/maladies.owl#>
+    prefix ns2: <http://www.w3.org/2002/07/owl#>
+    prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+    prefix xml: <http://www.w3.org/XML/1998/namespace>
+    SELECT ?id ?nom ?prenom ?age ?sexe ?enceinte ?poids ?taille ?temp ?maladie
+    WHERE{{
+        ?code rdf:type ns1:Patient .
+        ?code ns1:ID ?id .
+        ?code ns1:Nom ?nom .
+        ?code ns1:Prenom ?prenom .
+        ?code ns1:Age ?age .
+        ?code ns1:Sexe ?sexe .
+        ?code ns1:est_enceinte ?enceinte .
+        ?code ns1:poids ?poids .
+        ?code ns1:taille ?taille .
+        ?code ns1:Temperature ?temp .
+        ?code rdf:type ns1:Patient .
+        ?code ns1:{prop} ?maladie
+        FILTER regex(?maladie, '.pneumonie')
+    }}
+    """
+    resultat = graph.query(requete)
+    for res in list(resultat):
+        for x in list(res):
+            print(x)
+        print('___________________________')
+
+
+get_props('est_atteint_de')
